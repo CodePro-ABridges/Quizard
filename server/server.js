1ba78f6289cfc1fae1e769bf279484cdd9a9f638
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-15 01:57:12 -0600

    debug

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ import { authMiddleware } from "./utils/auth.js";
 
     app.use("/static", express.static(path.join(__dirname, "..", "client")));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+      res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
     });
 
     await db();
