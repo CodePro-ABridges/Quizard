cbf926256df0b269f580814efa9150cb096af80c
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-15 00:07:12 -0600

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -44,10 +44,10 @@ import { authMiddleware } from "./utils/auth.js";
 
     app.use(
       "/static",
-      express.static(path.join(__dirname, "..", "client", "dist")),
+      express.static(path.join(__dirname, "..", "client", "build")),
     );
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
     });
 
     await db();
