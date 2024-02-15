762580e5362dec40072b9214a7b211c2048dac91
CodePro-ABridges <arthurbridges08@yahoo.com>
2024-02-14 23:29:28 -0600

    Deployment to Render

--- server/server.js
+++ server/server.js
@@ -1,25 +1,54 @@
 import express from "express";
+import helmet from "helmet";
 import { createServer } from "http";
 import { ApolloServer } from "apollo-server-express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import db from "./config/connection.js";
-import initializeSocketIo from "./socketServer.js";
+// import initializeSocketIo from "./socketServer.js";
 import cors from "cors";
 import { authMiddleware } from "./utils/auth.js";
-import Profile from "./models/Profile.js";
-import Game from "./models/Game.js";
-import Quiz from "./models/Quiz.js";
+// import Profile from "./models/Profile.js";
+// import Game from "./models/Game.js";
+// import Quiz from "./models/Quiz.js";
 
 (async () => {
   try {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
     const app = express();
     const PORT = process.env.PORT || 3001;
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
 
     app.use(cors());
-    app.use(express.static(path.join(__dirname, "client", "build")));
+
+    app.use(
+      helmet.contentSecurityPolicy({
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          styleSrc: [
+            "'self'",
+            "https://*.googleapis.com",
+            "'unsafe-inline'",
+            "https://cdn.jsdelivr.net",
+          ],
+          fontSrc: ["'self'", "*"],
+          imgSrc: ["'self'", "*"],
+          connectSrc: ["'self'", "*"],
+        },
+        reportOnly: false,
+      }),
+    );
+
+    app.use(
+      "/static",
+      express.static(path.join(__dirname, "..", "client", "dist")),
+    );
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+    });
 
     await db();
     console.log("Connected to MongoDB");
@@ -34,10 +63,10 @@ import Quiz from "./models/Quiz.js";
     apolloServer.applyMiddleware({ app });
 
     const httpServer = createServer(app);
-    initializeSocketIo(httpServer);
-    const profile = new Profile();
-    const game = new Game();
-    const quiz = new Quiz();
+    // initializeSocketIo(httpServer);
+    // const profile = new Profile();
+    // const game = new Game();
+    // const quiz = new Quiz();
     httpServer.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
